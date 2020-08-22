b17183f1a11e900ebf2b9444e24ce4242acf6ff1
Stephen Keating <stephen.keating@gmail.com>
2020-08-21 22:42:47 -0400

    remove console logs

--- src/components/GuessAlgorithm.js
+++ src/components/GuessAlgorithm.js
@@ -5,26 +5,23 @@ const GuessAlgorithm = (trackName, guess) => {
   let lowercaseTrackName = trackName.toLowerCase()
   
   // Strip punctuation, spaces, and capitalization from track titles and guess to make guessing easier
-  let lowercaseTrackNameBeforePunctuation = lowercaseTrackName.split(/[(,-]|pt./)[0].replace(/[^\w]/g, '')
-  // let currentTrackSplitAtComma = lowercaseTrackName.split(',')[0].replace(/[^\w]/g, '')
-  // let currentTrackBeforeDash = lowercaseTrackName.split('-')[0].replace(/[^\w]/g, '')
-  // let currentTrackBeforeParenthesis = lowercaseTrackName.split('(')[0].replace(/[^\w]/g, '')
+  // removing the comma from the split to make guessing a little bit harder
+  let lowercaseTrackNameBeforePunctuation = lowercaseTrackName.split(/[(-]|pt./)[0].replace(/[^\w]/g, '')
   let lowercaseTrackNameBetweenParenthesis = trackName.split(/[()]+/)[1] ? lowercaseTrackName.split(/[()]+/)[1].replace(/[^\w]/g, '') : trackName
-  // let currentTrackBeforePtPeriod = lowercaseTrackName.split('Pt.')[0].replace(/[^\w]/g, '')
-  // let currentTrackAfterParenthesis = trackName.split('(')[1].replace(/[^\w]/g, '').toLowerCase()
-  let jarowBeforePunctuationScore = distance(guess.split('(')[0].split('-')[0].replace(/[^\w]/g, ''), trackName.split('(')[0].split('-')[0].replace(/[^\w]/g, ''), { caseSensitive: false })
+  let jarowBeforePunctuationScore = distance(guess.split(/[(-]|pt./)[0].replace(/[^\w]/g, ''), lowercaseTrackNameBeforePunctuation, { caseSensitive: false })
+  let jarowBetweenParenthesisScore = distance(guess.replace(/[^\w]/g, ''), lowercaseTrackNameBetweenParenthesis, { caseSensitive: false })
   let jarowWholeStringScore = distance(guess, trackName, { caseSensitive: false })
-  console.log(trackName, guess, lowercaseTrackName, lowercaseTrackNameBeforePunctuation, jarowBeforePunctuationScore, jarowWholeStringScore)
-  // consider implementing a JS Switch
+
+  // console.log(lowercaseTrackName, guess, lowercaseTrackNameBeforePunctuation, lowercaseTrackNameBetweenParenthesis, jarowBeforePunctuationScore, jarowBetweenParenthesisScore, jarowWholeStringScore)
+  
   if ( 
-    // guess === currentTrackSplitAtComma
-    // || guess === currentTrackBeforeDash 
-    // || guess === currentTrackBeforeParenthesis 
-    // || guess === currentTrackBeforePtPeriod
-    guess === lowercaseTrackNameBeforePunctuation
-    || guess === lowercaseTrackNameBetweenParenthesis 
-    || jarowBeforePunctuationScore > .91
-    || jarowWholeStringScore > .89
+    // consider implementing a JS Switch
+    // guess === lowercaseTrackNameBeforePunctuation|| 
+    // guess === lowercaseTrackNameBetweenParenthesis || 
+    jarowBeforePunctuationScore > .91 || 
+    jarowWholeStringScore > .89 ||
+    jarowBetweenParenthesisScore > .95
+
     ) {
       return true
     } else {
